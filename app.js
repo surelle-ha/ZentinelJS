41982df8e7fa7b17f34b168794d3e0d5a76da40e
Katherine Maamo <katherinemaamo7@gmailcom>
2024-06-14 20:44:01 +0800

    Webhook and RateLimit
    Implemented ratelimiter and webhook for reset
    
    Closing Feature

--- app.js
+++ app.js
@@ -1,14 +1,21 @@
+/*  _____________________________________
+
+                ZENTINEL JS
+    _____________________________________
+*/  
+require('module-alias/register')
+
 /* Import Server Modules */
 const express = require("express");
 const serveIndex = require('serve-index');
 
 /* Import Configurations */
-const { connectDB: sql_setup, sequelize } = require("./config/sql.js");
-const { connectDB: nosql_setup } = require("./config/nosql.js");
-const { RateLimit } = require("./config/ratelimiter.js");
-const { cors, cors_options } = require("./config/cors");
-const { helmet } = require("./config/helmet");
-const { loggerPino } = require("./config/logger");
+const { connectDB: sql_setup, sequelize } = require("@config/sql.js");
+const { connectDB: nosql_setup } = require("@config/nosql.js");
+const { RateLimit } = require("@config/ratelimiter.js");
+const { cors, cors_options } = require("@config/cors");
+const { helmet } = require("@config/helmet");
+const { loggerPino } = require("@config/logger");
 
 /* Setup Express Application */
 const app = express();
@@ -30,12 +37,15 @@ app.use("/", express.static("public"));
 app.use('/storage', express.static('storage'), serveIndex('storage', {'icons': true }));
 app.use('/orm-builder', express.static('storage/sequelize-ui'));
 
+app.config = {};
+app.config.RateLimit = RateLimit;
+
 /* Routes: Web, API */
-require("./app/utilities/__i.js")(app);
-require("./app/services/__i.js")(app);
-require("./app/models/__i.js")(app, sequelize);
-require("./app/middlewares/__i.js")(app);
-require("./app/controllers/__i.js")(app);
-require("./app/routes/__i.js")(app);
+require("@app/utilities/__i.js")(app);
+require("@app/services/__i.js")(app);
+require("@app/models/__i.js")(app, sequelize);
+require("@app/middlewares/__i.js")(app);
+require("@app/controllers/__i.js")(app);
+require("@app/routes/__i.js")(app);
 
 module.exports = app;
