afb984bfe324d4be22b1972edaa714bceef39496
surelle-ha <0110harold@gmail.com>
2024-06-15 09:15:46 +0800

    test updates

--- tests/Basic.test.js
+++ tests/Basic.test.js
@@ -1,7 +1,24 @@
-const request = require('supertest');
-const app = require('../app.js');
+const request = require("supertest");
+const app = require("../app"); // Path to your Express application
 
-describe('Basic', () => {
-    // Compose your test set here.
+describe("GET Extension Tools", done => {
+  it("should return 200 OK", done => {
+    request(app)
+      .get("/")
+      .end((err, response) => {
+        if (err) return done(err);
+        expect(response.statusCode).toBe(200);
+        done();
+      });
+  });
+
+  it("should return 200 OK", done => {
+    request(app)
+      .get("/orm-builder/")
+      .end((err, response) => {
+        if (err) return done(err);
+        expect(response.statusCode).toBe(200);
+        done();
+      });
+  });
 });
-    
