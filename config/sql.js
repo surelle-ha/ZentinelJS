51987d3627d90fffb187b97ef8c124674e41971c
surelle-ha <bsit.eustahc@gmail.com>
2024-07-19 09:13:03 +0800

    fixed: deployment

--- config/sql.js
+++ config/sql.js
@@ -3,6 +3,7 @@ const ENV = require("./environments");
 
 const sequelize = new Sequelize(ENV.DATABASE_NAME, ENV.DATABASE_USER, ENV.DATABASE_PASS, {
 	host: ENV.DATABASE_HOST,
+	port: ENV.DATABASE_PORT,
 	dialect: ENV.DATABASE_TYPE,
 	logging: false
 });
