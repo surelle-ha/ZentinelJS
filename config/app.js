72072f6bf3e6205947f163f63b8216bafc000fd2
Katherine Maamo <katherinemaamo7@gmailcom>
2024-07-26 16:21:03 +0800

    Your commit message

--- config/app.js
+++ config/app.js
@@ -7,9 +7,9 @@ require("module-alias/register");
 
 /* Setup Express Application */
 const config = require("@/zentinel.config.js").config;
-const app = require("@config/__gem")(config);
+const app = require("@config/_engine")(config);
 
 /* Routes: Web, API */
-const globalMiddleware = require("@app/__i")(app);
+require("@app/__i")(app);
 
 module.exports = app;
